bf4eaa64bed6723338f28524ddfb5ca63506970a
DausterBarbosa <dausterbarbosa12@gmail.com>
2020-11-12 12:44:20 -0300

    json configurado

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
+import "reflect-metadata";
+
 import express from "express";
+
+import "./database";
 
 import Routes from "./routes";
 
 export const app = express();
+
+app.use(express.json());
 
 app.use(Routes);
