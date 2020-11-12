b4ef43229c5272449843c70f42550637c922ea74
DausterBarbosa <dausterbarbosa12@gmail.com>
2020-11-12 20:43:43 -0300

    migration de deiveryman criado

---
+++ src/database/migration/1605205184706-create-deliverymans.ts
@@ -0,0 +1,59 @@
+import {MigrationInterface, QueryRunner, Table, TableColumn, TableForeignKey} from "typeorm";
+
+export class createDeliverymans1605205184706 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable( new Table({
+            name: "deliveryman",
+            columns: [
+                {
+                    name: "id",
+                    type: "int",
+                    isPrimary: true,
+                    isGenerated: true,
+                    unsigned: true,
+                    generationStrategy: "increment",
+                },
+                {
+                    name: "name",
+                    type: "varchar",
+                    isNullable: false,
+                },
+                {
+                    name: "email",
+                    type: "varchar",
+                    isNullable: false,
+                },
+                {
+                    name: "created_at",
+                    type: "timestamp",
+                    isNullable: false,
+                    default: "now()",
+                },
+                {
+                    name: "updated_at",
+                    type: "timestamp",
+                    isNullable: false,
+                    default: "now()",
+                }
+            ]
+        }));
+
+        await queryRunner.addColumn("deliveryman", new TableColumn({
+            name: "avatar_id",
+            type: "int",
+        }));
+
+        await queryRunner.createForeignKey("deliveryman", new TableForeignKey({
+            columnNames: ["avatar_id"],
+            referencedTableName: "avatar",
+            referencedColumnNames: ["id"],
+            onDelete: "CASCADE",
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("deliveryman");
+    }
+
+}
