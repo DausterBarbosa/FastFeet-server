ad1c280365cc19e79202b39f91387214cd8c13e6
DausterBarbosa <dausterbarbosa12@gmail.com>
2020-11-12 12:44:40 -0300

    rotas feitas

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,19 @@
 import {Router} from "express";
 
+import AuthMiddleware from "./middleware/AuthMiddleware";
+
+import UserController from "./app/controllers/UserController";
+import SessionController from "./app/controllers/SessiconController";
+import RecipientsController from "./app/controllers/RecipientsController";
+
 const route = Router();
 
-route.get("/", (req, res) => {
-    return res.send("adjfaçskdfj")
-});
+route.get("/createuser", UserController.create);
+route.post("/session", SessionController.create);
+
+route.use(AuthMiddleware);
+
+route.post("/recipient", RecipientsController.create);
+route.put("/recipient/:id", RecipientsController.update);
 
 export default route;
