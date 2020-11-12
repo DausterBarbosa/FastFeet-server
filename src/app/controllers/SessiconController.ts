656a9ac0c36e92973222f3c57a1af3ebaec061a0
DausterBarbosa <dausterbarbosa12@gmail.com>
2020-11-12 12:39:56 -0300

    Session Controller feito

---
+++ src/app/controllers/SessiconController.ts
@@ -0,0 +1,48 @@
+import {getRepository} from "typeorm";
+import User from "../entity/User";
+
+import jwt from "jsonwebtoken";
+import config from "../../config/token";
+
+import * as yup from "yup";
+
+import {Request, Response} from "express";
+
+class SessionController{
+    async create(req:Request, res:Response){
+        const schema = yup.object().shape({
+            email: yup.string().email().required(),
+            password: yup.string().required(),
+        });
+
+        if(! (await schema.isValid(req.body))){
+            return res.status(401).json({"error": "Validation fail"});
+        }
+
+        const {email, password} = req.body;
+
+        const UserRepository = getRepository(User);
+        const user = await UserRepository.findOne({where: {email}});
+
+        if(!user){
+            return res.status(401).json({"error": "Email not found."});
+        }
+
+        if(! await user.passwordValidate(password)){
+            return res.status(401).json({"error": "Password do not match."});
+        }
+
+        const token = jwt.sign({id: user.id}, config.secret, {
+            expiresIn: config.time,
+        });
+
+        return res.status(200).json({
+            name: user.name,
+            email: user.email,
+            token,
+        });
+
+    }
+}
+
+export default new SessionController;
