d8421b1a4df5456f1fc9a37194215f7314742325
DausterBarbosa <dausterbarbosa12@gmail.com>
2020-11-12 14:44:44 -0300

    rotas de avatar feita

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,16 @@
 import {Router} from "express";
+import multer from "multer";
 
 import AuthMiddleware from "./middleware/AuthMiddleware";
+import MulterConfig from "./config/multer";
 
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessiconController";
 import RecipientsController from "./app/controllers/RecipientsController";
+import AvatarController from "./app/controllers/AvatarController";
 
 const route = Router();
+const upload = multer({storage: MulterConfig});
 
 route.get("/createuser", UserController.create);
 route.post("/session", SessionController.create);
@@ -15,5 +19,7 @@ route.use(AuthMiddleware);
 
 route.post("/recipient", RecipientsController.create);
 route.put("/recipient/:id", RecipientsController.update);
+
+route.post("/avatar", upload.single("avatar"),AvatarController.create);
 
 export default route;
