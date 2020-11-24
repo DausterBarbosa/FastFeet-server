2ea4e018378a89c6ca44ac2a7b208c0c2f78bd84
DausterBarbosa <dausterbarbosa12@gmail.com>
2020-11-24 15:41:10 -0300

    novas rotas adicionadas

--- src/routes.ts
+++ src/routes.ts
@@ -9,12 +9,25 @@ import SessionController from "./app/controllers/SessiconController";
 import RecipientsController from "./app/controllers/RecipientsController";
 import AvatarController from "./app/controllers/AvatarController";
 import DeliveryManController from "./app/controllers/DeliveryManController";
+import SignatureController from "./app/controllers/SignatureController";
+import OrderController from "./app/controllers/OrderController";
+import DeliveryWaitingController from "./app/controllers/DeliveryWaitingController";
+import DeliveryDoneController from "./app/controllers/DeliveryDoneController";
+import StartDateController from "./app/controllers/StartDateController";
+import EndDateController from "./app/controllers/EndDateController";
 
 const route = Router();
 const upload = multer({storage: MulterConfig});
 
 route.get("/createuser", UserController.create);
 route.post("/session", SessionController.create);
+
+route.get("/deliveryman/:id/deliveries/waiting", DeliveryWaitingController.index);
+route.get("/deliveryman/:id/deliveries/done", DeliveryDoneController.index);
+route.post("/deliveryman/:deliverymanid/order/:orderid/start_date", StartDateController.create);
+route.post("/deliveryman/:deliverymanid/order/:orderid/end_date", EndDateController.create);
+
+route.post("/signature", upload.single("signature"), SignatureController.create);
 
 route.use(AuthMiddleware);
 
@@ -27,5 +40,10 @@ route.post("/deliveryman", DeliveryManController.create);
 route.get("/deliveryman", DeliveryManController.index);
 route.put("/deliveryman/:id", DeliveryManController.update);
 route.delete("/deliveryman/:id", DeliveryManController.delete);
+
+route.post("/order", OrderController.create);
+route.get("/order", OrderController.index);
+route.put("/order/:id", OrderController.update);
+route.delete("/order/:id", OrderController.delete);
 
 export default route;
