2927992a5b15e892f6ae907bd04b1ec0f209ade0
DausterBarbosa <dausterbarbosa12@gmail.com>
2020-11-24 15:32:23 -0300

    sistema de filas feita

---
+++ src/Queue/queue.ts
@@ -0,0 +1,39 @@
+import Bull from "bull";
+
+import redisConfig from "../config/redis";
+
+import OrderRegisterMail from "../app/jobs/OrderRegisterMail";
+
+class Queue{
+    private jobs = [OrderRegisterMail];
+    private queues = {};
+
+    constructor(){
+        this.init();
+    }
+
+    private init(){
+        this.jobs.forEach(({key, handle}) => {
+            this.queues[key] = {
+                queue: new Bull(key, {redis: redisConfig}),
+                handle,
+            };
+        });
+    }
+
+    async add(key, data){
+        return await this.queues[key].queue.add(data);
+    }
+
+    process(){
+        this.jobs.forEach(job => {
+            const {queue, handle} = this.queues[job.key];
+
+            queue.on("failed", (job, result) => {
+                console.log(job, result);
+            }).process(handle);
+        });
+    }
+}
+
+export default new Queue;
