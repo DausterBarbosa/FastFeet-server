4a80c2a4b258ed5e44c41a1f947ccea0f4a4f56b
DausterBarbosa <dausterbarbosa12@gmail.com>
2020-11-24 15:38:02 -0300

    job da fila feito

---
+++ src/app/jobs/OrderRegisterMail.ts
@@ -0,0 +1,28 @@
+import Email from "../../Email/email";
+
+class OrderRegisterMail{
+    get key(){
+        return "OrderRegisterMail";
+    }
+
+    async handle(data){
+        return await Email.sendEmail({
+            from: '"Fred Foo 👻" <foo@example.com>',
+            to: data.data.deliveryman.email,
+            subject: "Encomenda",
+            template: "order-create",
+            context: {
+                deliveryMan: data.data.deliveryman.name,
+                product: data.data.product,
+                recipient: data.data.recipient.name,
+                state: data.data.recipient.state,
+                city: data.data.recipient.city,
+                cep: data.data.recipient.cep,
+                street: data.data.recipient.street,
+                complement: data.data.recipient.complement,
+            }
+        });
+    }
+}
+
+export default new OrderRegisterMail;
