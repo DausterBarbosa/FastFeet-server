16221b720bda3aa84f7a0a9cf26c9387f6a01e41
DausterBarbosa <dausterbarbosa12@gmail.com>
2020-11-26 11:31:18 -0300

    job de cancelamento de email feito

---
+++ src/app/jobs/OrderCanceledMail.ts
@@ -0,0 +1,23 @@
+import Email from "../../Email/email";
+
+class OrderCancelledMail {
+    get key(){
+        return "OrderCanceledMail";
+    }
+
+    async handle(data){
+        await Email.sendEmail({
+            from: '"Fred Foo 👻" <foo@example.com>',
+            to: data.data.deliveryman.email,
+            subject: "Encomenda cancelada",
+            template: "order-canceled",
+            context: {
+                deliveryMan: data.data.deliveryman.name,
+                product: data.data.product,
+                recipient: data.data.recipient.name,
+            }
+        });
+    }
+}
+
+export default new OrderCancelledMail;
